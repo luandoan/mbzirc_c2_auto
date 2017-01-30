eeb3248d6340766f46c6897ec8c95dbeb50a2e23
luandoan <luandoan263@gmail.com>
2017-01-30 10:18:17 -0500

    joy_ptz node fixed

--- sunba/script/sunba_control.py
+++ sunba/script/sunba_control.py
@@ -7,12 +7,12 @@ import time
 import cv2
 from sensor_msgs.msg import Joy
 
-def control_sunba(vertical, horizontal, zoom):
-    if(horizontal>0):
+def control_sunba(vertical, horizontal, zoom, stop):
+    if(horizontal<0):
 	goRight()
 	time.sleep(0.01)
 	print("PTZ pan to the right")
-    elif(horizontal<0):
+    elif(horizontal>0):
 	goLeft()
 	time.sleep(0.01)
 	print("PTZ pan to the left")
@@ -33,7 +33,7 @@ def control_sunba(vertical, horizontal, zoom):
 	time.sleep(0.01)
 	print("PTZ zooming out")
     elif(stop==1):
-	stop()
+	stopPTZ()
 
 def goRight():
     ip_right = 'http://10.10.10.13/?command=ptz_req&req=start&param=directionleft&channel=1&stream=0'
@@ -41,8 +41,8 @@ def goRight():
     ip_stop = 'http://10.10.10.13/?command=ptz_req&req=stop&param=directionleft&channel=1&stream=0'
     URL_stop = ip_stop
     response = ul.urlopen(URL_right)
-    rospy.Duration(0.5)
-    response = ur.urlopen(URL_stop)
+    rospy.Duration(20.0)
+    response = ul.urlopen(URL_stop)
     #rospy.loginfo(fullURL)
 	
 def goLeft():
@@ -51,53 +51,69 @@ def goLeft():
     ip_stop = 'http://10.10.10.13/?command=ptz_req&req=stop&param=directionright&channel=1&stream=0'
     URL_stop = ip_stop    
     response = ul.urlopen(URL_left)
-    rospy.Duration(0.5)
-    response = ur.urlopen(URL_stop)
+    rospy.Duration(20.0)
+    response = ul.urlopen(URL_stop)
     #rospy.loginfo(fullURL)
 
 def lookUp():
     ip_up = 'http://10.10.10.13/?command=ptz_req&req=start&param=directiondown&channel=1&stream=0'
     URL_up = ip_up
+    ip_stop = 'http://10.10.10.13/?command=ptz_req&req=stop&param=directiondown&channel=1&stream=0'
+    URL_stop = ip_stop
     response = ul.urlopen(URL_up)
+    rospy.Duration(0.0)
+    response = ul.urlopen(URL_stop)
     #rospy.loginfo(fullURL)
 
 def lookDown():
     ip_down = 'http://10.10.10.13/?command=ptz_req&req=start&param=directionup&channel=1&stream=0'
     URL_down = ip_down
+    ip_stop = 'http://10.10.10.13/?command=ptz_req&req=stop&param=directionup&channel=1&stream=0'
+    URL_stop = ip_stop
     response = ul.urlopen(URL_down)
+    rospy.Duration(0.0)
+    response = ul.urlopen(URL_stop)
     #rospy.loginfo(fullURL)
 
 def zoomTile():
     ip_zoomIn = 'http://10.10.10.13/?command=ptz_req&req=start&param=zoomtile&channel=1&stream=0'
     URL_zoomIn = ip_zoomIn
     response = ul.urlopen(URL_zoomIn)
+    ip_stop = 'http://10.10.10.13/?command=ptz_req&req=stop&param=zoomtile&channel=1&stream=0'
+    URL_stop = ip_stop
+    rospy.Duration(0.0)
+    response = ul.urlopen(URL_stop)
     #rospy.loginfo(fullURL)
 
 def zoomWide():
     ip_zoomOut = 'http://10.10.10.13/?command=ptz_req&req=start&param=zoomwide&channel=1&stream=0'
     URL_zoomOut = ip_zoomOut
+    ip_stop = 'http://10.10.10.13/?command=ptz_req&req=stop&param=zoomwide&channel=1&stream=0'
+    URL_stop = ip_stop
     response = ul.urlopen(URL_zoomOut)
+    rospy.Duration(0.0)
+    response = ul.urlopen(URL_stop)
     #rospy.loginfo(fullURL)
 
-def stop():
+def stopPTZ():
     ip = 'http://10.10.10.13/?command=ptz_req&req=stop&param=directiondown&channel=1&stream=0'
     URL = ip
     response = ul.urlopen(URL)
 
 
 def callback(data):
-    rospy.loginfo(data)
+    #rospy.loginfo(data)
     horizontal = data.buttons[4]*data.buttons[5]*data.axes[6]
-    print "Horizontal: ", horizontal
+    #print "Horizontal: ", horizontal
     vertical = data.buttons[4]*data.buttons[5]*data.axes[7]
-    print "Vertical: ", vertical
-    zoom = data.buttons[4]*data.buttons[5]*data.axes[1]
-    print "Zoom: ", zoom
+    #print "Vertical: ", vertical
+    zoom = data.buttons[5]*data.buttons[3]*data.axes[7]
+    #print "Zoom: ", zoom
     stop = data.buttons[4]*data.buttons[5]*data.buttons[2]
-    print "Stop: ", stop
+    #print "Stop: ", stop
 
     # publish to control node
-    control_sunba(vertical, horizontal, zoom)
+    control_sunba(vertical, horizontal, zoom, stop)
 
 
 def joySunba():
